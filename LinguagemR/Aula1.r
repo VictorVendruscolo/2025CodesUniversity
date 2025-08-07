2776e32a24eee06be9812142c05da17e34d331f4
VictorVendruscolo <rechvvictor@gmail.com>
2025-08-07 15:52:44 -0400

    Atualização de Repositórios

---
+++ LinguagemR/Aula1.r
@@ -0,0 +1,20 @@
+# Aula 1, Aprendendo a Sintaxe da Linguagem
+
+# Fórmula de Celsius para Fahrenheit
+(f <- (25 * (9 / 5)) + 32)
+
+# Fórmula para calcular distância entre dois pontos
+x <- 1
+x2 <- 4
+y <- 2
+y2 <- 6
+
+(d <- sqrt((x2 - x)^2 + (y2 - y)^2))
+
+# Cálculo de Báskara
+a <- 1
+b <- -3
+c <- -4
+
+(x <- -b + (sqrt(b^2 - (4 * a * c)) / 2 * a))
+(x2 <- -b - (sqrt(b^2 - (4 * a * c)) / 2 * a))
---
+++ LinguagemR/Aula2.r
@@ -0,0 +1,7 @@
+mylist <- list(
+    name = "Joao",
+    age = 28,
+    grade = c(8.5, 9.0, 5.8),
+    approved = TRUE
+)
+mylist
